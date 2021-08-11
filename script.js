b19568802f102acb118a5b37315f75749b53dbfe
AnaPutkaradze <85496856+AnaPutkaradze@users.noreply.github.com>
2021-08-11 13:02:40 +0400

    Done

---
+++ script.js
@@ -0,0 +1,54 @@
+// // // // Task 1
+// // // let newArray = [5, 25, 89, 120, 36];
+// // // newArray.push('javascript', 'python');
+// // // newArray.unshift('html', 'css');
+// // // console.log(newArray);
+// // // newArray.pop();
+// // // newArray.shift();
+// // // console.log(newArray);
+
+
+// // // // Task 2
+// // // let array = ['portoxali', 'banani', 'msxali'];
+// // // console.log(array.length);
+// // // array.push('vashli', 'ananasi');
+// // // array.unshift('sazamtro');
+// // // console.log(array.length);
+// // // array.splice(2,0, 'mango');
+// // // console.log(array);
+// // // array.shift();
+// // // array.pop();
+// // // console.log(array.length);
+
+
+// // // Task 3
+// // let Frontend = {
+// //     HTML: 'css',
+// //     bootstrap: 'scss',
+// //     javascript: 871
+// // }
+// // Object.keys(Frontend).forEach( (item) => {
+// //     console.log(item);
+// // })
+
+// // // Task 4
+// // let location = {
+// //     loction: 'Tbilisi',
+// //     place: 'Botanical garden',
+// //     Year: 1665
+// // }
+
+// // for (let i of Object.values(location)) {
+// //     console.log(i);
+// // }
+
+// // // Task 5
+// // let array5 = [12, 25, 3, 6, 8, 14, 7, 23];
+
+// // let newarray = array5.map( (x) => x / 3 );
+// // console.log(newarray);
+
+// // Task 6
+// let array1 = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+// let newArray = array1.filter( (x) => typeof x == 'number');
+// console.log(newArray);
